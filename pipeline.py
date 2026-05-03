99777451e3eda407defb3fc54ff8abdeac55df22
Florecita Wenny <h1101241039@student.untan.ac.id>
2026-05-03 16:33:32 +0700

    fix import nama flow dim_toko dan dim_waktu

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,5 @@
 from prefect import flow
 
-# Import semua flow dari file ETL
 from dim_pelanggan_etl import etl_dim_pelanggan_flow
 from dim_produk_etl import etl_dim_produk_flow
 from dim_toko_etl import flow_etl_dim_toko
@@ -10,16 +9,13 @@ from fact_sales_etl import etl_fact_sales_flow
 from fact_delivery_etl import etl_fact_delivery_flow
 from fact_target_sales_etl import etl_fact_target_sales_flow
 
-@flow(name="Pipeline ETL Sederhana", description=" flow ")
+@flow(name="Pipeline ETL Sederhana", description="Contoh flow untuk materi kuliah SI")
 def etl_flow():
-    # Jalankan semua dimensi 
     etl_dim_pelanggan_flow()
     etl_dim_produk_flow()
     flow_etl_dim_toko()
     etl_dim_kurir_flow()
     flow_etl_dim_waktu()
-    
-    # jalankan fakta
     etl_fact_sales_flow()
     etl_fact_delivery_flow()
     etl_fact_target_sales_flow()
